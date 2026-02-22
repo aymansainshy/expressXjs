2141e86e5b79f83750684d557acaf531f4030bbb
aymansainshy <aymansainshy@gmail.com>
2026-02-22 11:57:18 +0400

    lock the app after bootstrap

--- packages/framework/index.ts
+++ packages/framework/index.ts
@@ -1,3 +1,4 @@
 export { ExpressX } from './expressX';
 export { ExpressXFactory } from '../framework/expressX.factory';
 export { ExpressXApp, Request, Response, NextFn, Ctx } from '../framework/types'
+export { OnInitExpressXApp, OnInitMiddleware } from '../framework/onIniteSetup';
