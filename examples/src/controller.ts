17a0bac30c1741d7414c2e66ecc3541834aad926
aymansainshy <aymansainshy@gmail.com>
2026-09-05 02:58:53 +0400

    fix: update UserController and AppExceptionHandler to improve error handling and response structure

--- examples/src/controller.ts
+++ examples/src/controller.ts
@@ -13,8 +13,9 @@ import {
   UseGuards,
   UseInterceptors,
   UseMiddlewares,
+  HttpResponse,
+  HttpErrorResponse,
 } from '@expressxjs/core';
-import { HttpResponse } from '@expressxjs/core';
 import { ResponseEnvelopeInterceptor } from './interceptors';
 import { JWTAuthGuard, JWTAuthGuard2 } from './auth.guard';
 import { AuthMiddleware, LoggerMiddleware } from './middlewares';
@@ -63,7 +64,9 @@ export class UserController {
   @UseInterceptors(ResponseEnvelopeInterceptor)
   @UseMiddlewares(LoggerMiddleware, AuthMiddleware, 1)
   @UseGuards(JWTAuthGuard2, 4)
-  @StatusCode(200)
+  // This the status code that will be used if the controller method returns a plain value (not an HttpResponse or HttpErrorResponse).
+  // or if the result is modified by an interceptor and the interceptor does not return an HttpResponse or HttpErrorResponse.
+  @StatusCode(209)
   public async getUsers(
     @Ctx()
     ctx: HttpContext,
@@ -77,9 +80,12 @@ export class UserController {
       // throw new Error("This is a test error to demonstrate global exception handling");
       const userList: User[] = await this.userService.getUserLis();
       console.log(userList);
-      // return HttpResponse.ok(userList); // new HttpResponse().status(345).body(userList);
-      // return { message: "Users retrieved successfully", data: userList }; // @StatusCode(200)
-      ctx.res.status(200).json({ message: 'Users retrieved successfully', data: userList });
+      // return HttpResponse.ok(userList);
+      // return new HttpResponse().status(345).body(userList);
+      // return { message: 'Users retrieved successfully', data: userList }; // @StatusCode(200)
+      // ctx.res.status(200).json({ message: 'Users retrieved successfully', data: userList });
+      // return new HttpResponse(201, { message: 'Users retrieved successfullyd', data: userList });
+      return new HttpErrorResponse(400, { message: 'Bad Request', error: 'Invalid parameters' });
     } catch (error) {
       // next(error);
       // This will be caught by the global exception handler
